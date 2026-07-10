5e159ca2f207658e220b59a13bb348e18f81cafc
Anil <anilkumar.sunkara@citrix.com>
2026-07-10 17:05:16 +0530

    .

--- BMRC_Framework_New/pages/base_page.py
+++ BMRC_Framework_New/pages/base_page.py
@@ -1,5 +1,6 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
 
@@ -7,10 +8,28 @@ class BasePage:
         self.driver = driver
 
     def click(self, locator):
-        self.driver.find_element(*locator).click()
+        WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable(locator)
+        ).click()
 
     def select_visible_text(self, locator, text):
-        Select(self.driver.find_element(*locator)).select_by_visible_text(text)
+
+        dropdown = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(locator)
+        )
+
+        select = Select(dropdown)
+
+        print("\nAvailable options:")
+        for option in select.options:
+            print(repr(option.text))
+
+        select.select_by_visible_text(text.strip())
 
     def select_value(self, locator, value):
-        Select(self.driver.find_element(*locator)).select_by_value(value)
+
+        dropdown = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(locator)
+        )
+
+        Select(dropdown).select_by_value(value)
